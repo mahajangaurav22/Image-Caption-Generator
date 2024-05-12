376b8a0d01a4bdf061921a76187b4b622f91f405
GAURAV MAHAJAN <77409117+mahajangaurav22@users.noreply.github.com>
2024-05-13 00:29:35 +0530

    Create web2.py

---
+++ web2.py
@@ -0,0 +1,119 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+
+
+
+
+
+st.write("## Generate Image Caption")
+st.write(
+    ":dog: Try uploading an image to generate a narrative for that image"
+)
+st.sidebar.write("## Upload and download :gear:")
+
+
+
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        # fix_image(upload=my_upload)
+        pass
+else:
+#     fix_image("./zebra.jpg")
+    pass
+
+
+
+from transformers import GPT2TokenizerFast, ViTImageProcessor, VisionEncoderDecoderModel
+from torch.utils.data import Dataset
+from torchtext.data import get_tokenizer
+import requests
+import torch
+import numpy as np
+from PIL import Image
+import pickle
+# from torchvision import transforms
+# from datasets import load_dataset
+# import torch.nn as nn
+import matplotlib.pyplot as plt
+import os
+from tqdm import tqdm
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+#model_raw = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+#for google collab use this code
+# model_raw = torch.load('/content/model_test_whole.pth')
+
+#for local machine running use this
+# model_raw = torch.load('/Users/gauravmahajan/Downloads/model_test_whole_2may.pth')
+# model_raw.eval()
+
+
+# for importing model through
+import requests
+
+model_url = "https://drive.google.com/file/d/19bC0Hqc58HAjqXRc0P-qIp4sv3LKXp88/view?usp=drive_link"
+response = requests.get(model_url)
+with open("model_test_whole_2may.pth", "wb") as file:
+    file.write(response.content)
+
+model_raw = torch.load("model_test_whole_2may.pth")
+
+
+
+
+
+
+#model_raw.load_state_dict(torch.load('/content/model_trained.pth'))
+image_processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer       = GPT2TokenizerFast.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+import requests
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def show_n_generate(file_path, greedy=True, model=model_raw):
+    try:
+        image = Image.open(file_path)
+        pixel_values = image_processor(image, return_tensors="pt").pixel_values
+        col1.write("Original Image :camera:")
+        col1.image(image)
+        
+   
+        
+
+        if greedy:
+            generated_ids = model.generate(pixel_values, max_new_tokens=30)
+        else:
+            generated_ids = model.generate(
+                pixel_values,
+                do_sample=True,
+                max_new_tokens=30,
+                top_k=5)
+        generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        col2.write(generated_text)
+        
+          
+
+    except Exception as e:
+        st.error("Please upload an image first.")
+
+# Example usage:
+# file_path = ""
+# show_n_generate(my_upload, greedy=False)
+if st.button("Generate Caption"):
+    # Call your function when the button is clicked
+    show_n_generate(my_upload, greedy=False)
+
+
